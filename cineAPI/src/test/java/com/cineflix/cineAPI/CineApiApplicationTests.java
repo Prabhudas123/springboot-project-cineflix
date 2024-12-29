6e90163b8bb1c47e201204124baf89a76f48e2e0
Prabhudas777 <kprasad13210@gmail.com>
2024-12-29 10:07:46 +0530

    Added properties in yml file

---
+++ cineAPI/src/test/java/com/cineflix/cineAPI/CineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineflix.cineAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
